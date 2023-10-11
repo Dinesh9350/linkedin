25e5cc1128a32ed17907d48a55811781887c2120
Dinesh <dineshsingh9350@gmail.com>
2023-10-11 19:50:31 +0530

    home page navbar added 3 fixe

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <nav className='flex sm:justify-between items-center whitespace-nowrap m-2 sm:m-1 mx-5 lg:mx-10 xl:mx-auto '>
+    <nav className='flex justify-between items-center whitespace-nowrap m-2 sm:m-1 mx-5 lg:mx-10 xl:mx-auto '>
       <div className='flex'>
         <Link to='/'>
           <img
@@ -25,9 +25,9 @@ const Header = () => {
           />
         </div>
       </div>
-      <div className='flex items-center justify-between ml-3 text-center'>
+      <div className='flex justify-center items-center mx-auto'>
         <Link to='/search' className='lg:hidden '>
-          <div className='list-none flex flex-col items-center justify-center px-2 sm:px-5'>
+          <div className='list-none flex flex-col items-center px-3 sm:px-5'>
             <img
               className='w-4 h-4'
               src='https://cdn-icons-png.flaticon.com/512/3031/3031293.png'
@@ -36,25 +36,25 @@ const Header = () => {
           </div>
         </Link>
         <Link to='/home'>
-          <div className='list-none flex flex-col items-center justify-center px-2 sm:px-5'>
+          <div className='list-none flex flex-col items-center px-3 sm:px-5'>
             <img className='w-6 h-6' src='/images/nav-home.svg' alt='home' />
             <span className='text-xs hidden sm:block'>Home</span>
           </div>
         </Link>
         <Link to='/home'>
-          <div className='list-none flex flex-col items-center justify-center px-2 sm:px-5'>
+          <div className='list-none flex flex-col items-center px-3 sm:px-5'>
             <img className='w-6 h-6' src='/images/nav-network.svg' alt='home' />
             <span className='text-xs hidden sm:block '>Network</span>
           </div>
         </Link>
         <Link to='/home'>
-          <div className='list-none flex flex-col items-center justify-center px-2 sm:px-5'>
+          <div className='list-none flex flex-col items-center px-3 sm:px-5'>
             <img className='w-6 h-6' src='/images/nav-jobs.svg' alt='home' />
             <span className='text-xs hidden sm:block '>Jobs</span>
           </div>
         </Link>
         <Link to='/home'>
-          <div className='list-none flex flex-col items-center justify-center px-2 sm:px-5'>
+          <div className='list-none flex flex-col items-center px-3 sm:px-5'>
             <img
               className='w-6 h-6'
               src='/images/nav-messaging.svg'
@@ -64,7 +64,7 @@ const Header = () => {
           </div>
         </Link>
         <Link to='/home'>
-          <div className='list-none flex flex-col items-center justify-center px-2 sm:px-5'>
+          <div className='list-none flex flex-col items-center px-2 sm:px-5'>
             <img
               className='w-6 h-6'
               src='/images/nav-notifications.svg'
@@ -74,18 +74,18 @@ const Header = () => {
           </div>
         </Link>
         <Link to='/home'>
-          <div className='list-none flex flex-col items-center justify-center px-2 sm:px-5'>
+          <div className='list-none flex flex-col items-center px-2 sm:px-5'>
             <img className='w-6 h-6' src='/images/nav-home.svg' alt='home' />
             <span className='text-xs hidden sm:block '>Me</span>
           </div>
         </Link>
-        <Link to='/home' className='hidden sm:block'>
-          <div className='list-none flex flex-col items-center justify-center px-2 sm:px-5'>
+        <Link to='/home' className='hidden lg:block'>
+          <div className='list-none flex flex-col items-center px-2 sm:px-5'>
             <img className='w-6 h-6' src='/images/nav-work.svg' alt='home' />
             <span className='text-xs hidden sm:block '>For Business</span>
           </div>
         </Link>
-        <Link to='/premium' className='hidden sm:block'>
+        <Link to='/premium' className='hidden lg:block'>
           <div className='text-xs text-red-800'>
             <h3>Get hired faster.</h3>
             <h3>Try Premium free</h3>
